41e93f243fa806549fb34ce0b99934a387bf8cf4
shadmanadman <adman.shadman@gmail.com>
2026-07-21 09:03:35 +0330

    Interface for converting text to vector

---
+++ vectora-core/src/commonMain/kotlin/org/shad/adman/vectora/core/embedding/EmbeddingEngine.kt
@@ -0,0 +1,15 @@
+package org.shad.adman.vectora.core.embedding
+
+import org.shad.adman.vectora.core.model.Vector
+
+// Defines the contract for converting text to vectors
+interface EmbeddingEngine {
+
+    suspend fun embed(
+        text: String
+    ): Vector
+
+    suspend fun embed(
+        texts: List<String>
+    ): List<Vector>
+}
